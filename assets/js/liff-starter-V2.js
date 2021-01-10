09ec227b75862c9717a22d851782e1b7580b5820
linebk <65086345+linebk@users.noreply.github.com>
2021-01-10 13:33:29 +0700

    Update liff-starter-V2.js

--- assets/js/liff-starter-V2.js
+++ assets/js/liff-starter-V2.js
@@ -76,7 +76,7 @@ function initApp() {
         liff.getProfile().then(profile => {
             const userDisplayName = profile.displayName;
             console.info("User name is", userDisplayName);
-            document.getElementById("greet").innerHTML = "Hi, " + userDisplayName + " ౼ Nice to meet you";
+            document.getElementById("greet").innerHTML = "สวัสดีครับ, " + userDisplayName + " ౼ ยินดีที่ได้ได้พบคุณ";
         }).catch((err) => {
             console.error("LIFF getProfile failed", err);
         });
